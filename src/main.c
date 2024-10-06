0d515735b5b2bc62e3c1ff21892f2e242a994dea
魔力铠甲 <2369004406@qq.com>
2024-10-06 19:18:25 +0800

    add TODO

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 int main(int argc, char** argv) {
     SystemInfo info;
     print_head_pic();
+    // TODO : add your user-arguments code here
+
     get_system_info(&info);
     print_system_info(&info);
     return 0;
